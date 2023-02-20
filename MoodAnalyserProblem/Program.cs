b0a5373926d236cec9578153329aeb7937110cce
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-02-20 22:30:53 +0530

    [Ayushi] Welcoming You

---
+++ MoodAnalyserProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program");
+            Console.ReadLine();
+        }
+    }
+}
